f41f15620f7725a4a74a586ce633e2e8c014367f
Andrew Rush <inarush0@gmail.com>
2026-06-07 17:30:42 -0400

    Chapter 2 Lesson 3

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -8,11 +8,21 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
         valid_target_dir = (
             os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
         )
+
         if not valid_target_dir:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-        if not os.path.isdir(directory):
+        if not os.path.isdir(target_dir):
             return f'Error: "{directory}" is not a directory'
 
-        return f'Success: "{directory}" is within the working directory'
+        dir_contents = os.listdir(target_dir)
+        results_list: list[str] = []
+        for dir in dir_contents:
+            path_to_file = "/".join([target_dir, dir])
+            is_dir = os.path.isdir(path_to_file)
+            file_size = os.path.getsize(path_to_file)
+            results_list.append(f"- {dir}: file_size={file_size}, is_dir={is_dir}")
+
+        return "\n".join(results_list)
+
     except Exception as e:
         return f"Error: {e}"
